af256d9af8ffc394a520e63064cb1cd5458bd174
Saurabh Joshi <saurabhsvj@gmail.com>
2017-09-04 17:36:05 -0700

    Fixes missing uploads directory

--- main.go
+++ main.go
@@ -1,11 +1,24 @@
 package main
 
 import (
+	"os"
+
 	"github.com/kataras/iris"
 	"github.com/saurabhsvj/photosharing/server"
 )
 
+const (
+	// LOCALUPLOADFOLDER holds the uploaded files in the local server
+	LOCALUPLOADFOLDER = "./uploads"
+)
+
 func main() {
+
+	// Ensure the uploads folder is available to create local files first
+
+	if _, err := os.Stat(LOCALUPLOADFOLDER); os.IsNotExist(err) {
+		os.Mkdir(LOCALUPLOADFOLDER, 0755)
+	}
 
 	app := iris.New()
 	app.Post("/upload", iris.LimitRequestBodySize(10<<20), server.UploadHandler)
